72f821f34f5b55ffbaad2fb14f361ae1a5d9037e
lgsosa <lg.sosa@alumno.um.edu.ar>
2024-09-30 21:00:58 -0300

    cosas tp10

--- frontend/src/app_biblioteca/src/app/app-routing.module.ts
+++ frontend/src/app_biblioteca/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { GestiondeusuarioComponent } from './pages/gestiondeusuario/gestiondeusuario.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { PrestamosComponent } from './pages/prestamos/prestamos.component';
+import { LibroComponent } from './pages/libro/libro.component';
 
 const routes: Routes = [
 
   {path:"home", component: HomeComponent},
-  {path:"", redirectTo: "/home", pathMatch:"full"}
+  {path:"error_page", component:ErrorPageComponent},
+  {path:"gestiondeusuario", component:GestiondeusuarioComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"prestamos", component:PrestamosComponent},
+  {path:"libro", component:LibroComponent},
+
+  {path:"", redirectTo: "/home", pathMatch:"full"},
+  {path:"**", redirectTo: "error_page"}
 
 ];
 
--- frontend/src/app_biblioteca/src/app/app.module.ts
+++ frontend/src/app_biblioteca/src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';  // Añadir RouterModule y Routes
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GestiondeusuarioComponent } from './pages/gestiondeusuario/gestiondeusuario.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { PrestamosComponent } from './pages/prestamos/prestamos.component';
+import { LibroComponent } from './pages/libro/libro.component';
+
+// Define tus rutas aquí
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'prestamos', component: PrestamosComponent },
+  { path: 'libro', component: LibroComponent },
+  { path: '**', component: ErrorPageComponent } // Ruta para manejar páginas no encontradas
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ErrorPageComponent,
+    NavComponent,
+    FooterComponent,
+    GestiondeusuarioComponent,
+    RegisterComponent,
+    PrestamosComponent,
+    LibroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)  // Añadir RouterModule con tus rutas
   ],
   providers: [],
   bootstrap: [AppComponent]
